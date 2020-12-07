2a58619c055935946e4454b59940af98e5aa766e
jasmin-alimanovic <62791380+jasmin-alimanovic@users.noreply.github.com>
2020-12-07 19:32:40 +0100

    Update DB.java

--- Source Packages/Baza Podataka/DB.java
+++ Source Packages/Baza Podataka/DB.java
@@ -1,1 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BazaPodataka;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Jasmin
+ */
+public class DB {
+    
+    private static final String url = "jdbc:mysql://localhost:3306/zdravstvena_organizacija?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String driverName = "com.mysql.cj.jdbc.Driver";
+    private static final String username = "root";
+    private static final String password = "jasmin123";
+    private static Connection con = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(driverName);
+            try {
+                con = DriverManager.getConnection(url, username, password);
+            } catch (SQLException ex) {
+                // log an exception. fro example:
+                System.out.println("Failed to create the database connection." + ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            // log an exception. for example:
+            System.out.println("Driver not found.");
+        }
+        return con;
+    }
+    
+    public void unosAmbulante(String naziv, String adresa)
+    {
+        Statement st = null;
+        String upit = "INSERT INTO ambulanta(naziv, adresa) VALUES('"+naziv+"', '"+adresa+"')";
+        try{
+            st = DB.getConnection().createStatement();
+            st.executeUpdate(upit);
+        }
+        catch(SQLException e)
+        {
+            System.err.println(e);
+        }
+    }
+}
