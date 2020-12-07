9cec9dbb8838e24d55fa1bfaac652e64996c97ed
jasmin-alimanovic <62791380+jasmin-alimanovic@users.noreply.github.com>
2020-12-07 20:54:39 +0100

    Add files via upload

---
+++ Source Packages/KLASE/Ambulanta.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KLASE;
+
+/**
+ *
+ * @author Jasmin
+ */
+public class Ambulanta {
+    
+    private int id;
+    private String naziv;
+    private String adresa;
+
+    public Ambulanta(int id, String naziv, String adresa) {
+        this.id = id;
+        this.naziv = naziv;
+        this.adresa = adresa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+    
+    
+}
