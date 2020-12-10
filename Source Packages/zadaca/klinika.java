18634166c220ab274818bc3aaf5c4b58bfede621
jasmin-alimanovic <62791380+jasmin-alimanovic@users.noreply.github.com>
2020-12-10 14:19:55 +0100

    Add files via upload

---
+++ Source Packages/zadaca/klinika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klinika;
+
+import GUI.Ambulanta;
+import GUI.Menu;
+
+/**
+ *
+ * @author Jasmin
+ */
+public class klinika {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Menu().setVisible(true);
+    }
+    
+}
+
+
